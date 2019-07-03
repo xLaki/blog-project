50f26e1c5329048ca31b38edb1990cd4213381c2
xLaki <brandonbedard12@gmail.com>
2019-07-03 10:18:26 -0400

    debug

--- app.js
+++ app.js
@@ -2,3 +2,13 @@
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
+const app = express();
+
+app.get('/', function(req, res){
+    console.log('bruh moment')
+    res.render('home    ')
+})
+
+app.listen(3000, function(req, res){
+console.log('server is live')
+})
